4ac27b6b8d1a8c0bfd842f98a20b52a15c5f516f
mpalado <144296180+mpalado@users.noreply.github.com>
2024-09-16 14:44:14 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: FirstScreen());
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello, World SHEESSHHH'),
+      ),
+    );
+  }
+}
